6ad53b3aaef8f03aed239602379a97b04e239d8c
Kevin13Wu <89596257+Kevin13Wu@users.noreply.github.com>
2025-06-07 16:41:58 -0400

    End of section 13

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,15 +1,47 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { InitService } from './core/services/init.service';
+import { defer, finalize, lastValueFrom } from 'rxjs';
+
+// async function initializeApp() {
+//   try {
+//     const initService = inject(InitService);
+//     return await lastValueFrom(initService.init());
+//   } finally {
+//     const splash = document.getElementById('initial-splash');
+//     if (splash) {
+//       splash.remove();
+//     }
+//   }
+// }
+
+function initializeApp() {
+  const initService = inject(InitService);
+  return defer(() => initService.init()).pipe(
+    finalize(() => {
+      const splash = document.getElementById('initial-splash');
+      if (splash) {
+        splash.remove();
+      }
+    })
+  );
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    provideAppInitializer(initializeApp),
   ],
 };
